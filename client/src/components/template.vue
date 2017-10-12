8129e5ced214e87faa21e03f7ffce3a1e5ac73c3
yu <yuchiu2002@hotmail.com>
2017-10-12 19:00:21 -0400

    implemented API service on client, and basic req/res for register API call on server

---
+++ client/src/components/template.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>register</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
