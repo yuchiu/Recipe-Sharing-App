5d84098c0e4c08006e2d7fd38d25953b2855d580
yu <yuchiu2002@hotmail.com>
2017-10-13 18:03:51 -0400

    added jwt for register method, so user gets the token once the account is registered

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -13,7 +13,11 @@ module.exports = {
     async register(req, res) {
         try {
             const user = await User.create(req.body)
-            res.send(user.toJSON())
+            const userJson = user.toJSON()
+            res.send({
+                user: userJson,
+                token: jwtSignUser(userJson)
+            })
         } catch (err){
             //email already exists
             res.status(400).send({
